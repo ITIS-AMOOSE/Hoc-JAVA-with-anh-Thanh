aaf9880db19ffa12d3509d1202f417f76c48e143
AMOOSE <DungDV.B23KH031@stu.ptit.edu.vn>
2025-07-30 21:41:05 +0700

    final

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Student{
     private String name;
     private int age;
@@ -37,5 +39,57 @@ class Student{
 }
 
 class StudentManger{
+    private Student[] students;
+    private int size = 0;
 
+    public StudentManger(int sz){
+        students = new Student[sz];
+    }
+
+    public void add(Student s){
+        if(size < students.length){
+            students[size++] = s;
+        }
+    }
+
+    public Student get(int i){
+        if(i >= 1 && i <= size){
+            return students[i - 1];
+        }
+        return null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        StudentManger SM = new StudentManger(n);
+
+        for(int i = 0; i < n; i++){
+            String name =  sc.nextLine();
+            int age = Integer.parseInt(sc.nextLine());
+            String address = sc.nextLine();
+            double math = Double.parseDouble(sc.nextLine());
+            double literature = Double.parseDouble(sc.nextLine());
+            double english = Double.parseDouble(sc.nextLine());
+
+            SM.add(new Student(name,age,address,math,literature,english));
+        }
+
+        int q = Integer.parseInt(sc.nextLine());
+        for(int i = 0; i < q; i++){
+            int idx = Integer.parseInt(sc.nextLine());
+            Student s = SM.get(idx);
+            System.out.println(s.getName());
+            System.out.println(s.getAge());
+            System.out.println(s.getAddress());
+            System.out.println(s.getMath());
+            System.out.println(s.getLiterature());
+            System.out.println(s.getEnglish());
+            System.out.printf("%.2f\n", s.average());
+            System.out.println(s.rank());
+        }
+        sc.close();
+    }
 }
